19eb7728a887dec9c408ac7d512d09d19f0f47c3
Ursula Leuwe <44085589+Welloprim@users.noreply.github.com>
2020-11-23 11:43:31 +0100

    Update app.py

--- apps-flask/app.py
+++ apps-flask/app.py
@@ -37,8 +37,8 @@ label_class = ['Hauts & Tee-shirts','Pantalons','Sweats et sweats à capuche','R
 
 # Load your own trained model
 model = load_model(MODEL_PATH)
-model._make_predict_function()          # Necessary
 #model = MobileNetV2(weights='imagenet')
+model._make_predict_function()          # Necessary
 print('Model loaded. Start serving...')
 
 
